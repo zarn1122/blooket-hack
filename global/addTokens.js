56a3a4bade84408ae4ff76e15ae4a57b8f40ce77
zarn1122 <91408688+zarn1122@users.noreply.github.com>
2021-11-02 08:16:58 +0300

    dd
    ss

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addTokens() {
-    const add_tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const add_tokens = Number(prompt('How many tokens do you want to add to your account? (10000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (add_tokens > 500) {
-        alert('You can add up to 500 tokens daily')
+    if (add_tokens > 10000) {
+        alert('You can add up to 10000 tokens daily')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -22,12 +22,12 @@ async function addTokens() {
         },
         body: JSON.stringify({
             addedTokens: add_tokens,
-            addedXp: 300,
+            addedXp: 10000,
             name: await getName(myToken)
         })
     });
 
-    if (response.status == 200) {
+    if (response.status == 1000) {
         alert(`${add_tokens} added to your account!`);
     } else {
         alert('An error occured.');
